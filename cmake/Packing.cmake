8657f690352c2dac0e60364c8b1b50b38dafb629
aceberg <1502200+aceberg@users.noreply.github.com>
2025-11-27 22:13:13 +0700

    Icon and .desktop file

--- cmake/Packing.cmake
+++ cmake/Packing.cmake
@@ -1,11 +1,19 @@
 install(TARGETS ${PROJECT_NAME} DESTINATION bin)
 
+install(FILES packaging/tray-pumpkin.desktop
+        DESTINATION share/applications)
+
+install(FILES packaging/icons/tray-pumpkin.png
+        DESTINATION share/icons/hicolor/48x48/apps)
+
 set(CPACK_GENERATOR "DEB;TGZ")
 set(CPACK_PACKAGE_NAME "tray-pumpkin")
-set(CPACK_PACKAGE_VERSION "0.1.2")
+set(CPACK_PACKAGE_VERSION "0.1.3")
 set(CPACK_PACKAGE_CONTACT "Andrew Erlikh <aceberg_a@proton.me>")
 set(CPACK_DEBIAN_PACKAGE_MAINTAINER "Andrew Erlikh")
-set(CPACK_DEBIAN_PACKAGE_DEPENDS "qtbase5-dev, libkf5notifications-dev, libyaml-cpp-dev")
+set(CPACK_DEBIAN_PACKAGE_DEPENDS
+    "libqt5widgets5, libqt5gui5, libqt5core5a, libkf5notifications5, libyaml-cpp0.7 | libyaml-cpp0.8"
+)
 set(CPACK_PACKAGE_DESCRIPTION_SUMMARY "configurable tray icon and menu")
 set(CPACK_PACKAGE_HOMEPAGE_URL "https://github.com/aceberg/TrayPumpkin")
 
